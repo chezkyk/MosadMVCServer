a55fe4d9b5a780544265a9b46bb65a7905cae665
chezkyk <y0548532123@gmail.com>
2024-08-26 12:36:23 +0300

    finish the controller

--- Controllers/AgentsController.cs
+++ Controllers/AgentsController.cs
@@ -15,35 +15,52 @@ namespace MosadMVCServer.Controllers
         }
         public async Task<IActionResult> Agents()
         {
-            List<Agent> agents = await ReturnsListOfAgents();
-            return View(agents);
+            var agents = await ReturnsListOfAgents();
+            var agentView = new List<AgentViews>();
+
+            foreach (var item in agents)
+            {
+                var amounOfKills = await ReturnsAmountOfKills(item.Id);
+                var timeLeft = await ReturnsTimeLeft(item.Id);
+                var newAgent = new AgentViews
+                {
+                    KillAmount = amounOfKills,
+                    TimeLeft = timeLeft,
+                    Agent = item
+                };
+                agentView.Add(newAgent);
+            }
+
+            return View(agentView);
         }
         public async Task<List<Agent>> ReturnsListOfAgents()
         {
             var agentsList = await _httpClient.GetFromJsonAsync<List<Agent>>("http://localhost:5125/agents");
             return agentsList;
         }
-		public async Task<IActionResult> Details(int id)
-		{
-			var mission = await GetMissionById(id);
-			return View(mission);
-		}
-		//
-		public async Task<int> ReturnsAmountOfKills(int id)
-		{
-			var amountOfKills = await _httpClient.GetFromJsonAsync<int>($"http://localhost:5125/missions/amountofkills/{id}");
-			return amountOfKills;
-		}
-		public async Task<double> ReturnsTimeLeft(int id)
-		{
-			var timeLeft = await _httpClient.GetFromJsonAsync<double>($"http://localhost:5125/timeleft/{id}");
-			return timeLeft;
-		}
-		//
-		public async Task<Mission> GetMissionById(int id)
-		{
-			Mission mission = await _httpClient.GetFromJsonAsync<Mission>($"http://localhost:5125/missions/get/{id}");
-			return mission;
-		}
-	}
+        public async Task<IActionResult> Details(int id)
+        {
+            var mission = await GetMissionById(id);
+            return View(mission);
+        }
+        //
+        public async Task<int> ReturnsAmountOfKills(int id)
+        {
+            var amountOfKills = await _httpClient.GetFromJsonAsync<int>($"http://localhost:5125/missions/amountofkills/{id}");
+            return amountOfKills;
+        }
+        public async Task<double> ReturnsTimeLeft(int id)
+        {
+            var timeLeft = await _httpClient.GetFromJsonAsync<double>($"http://localhost:5125/missions/timeleft/{id}");
+            
+            return timeLeft;
+        }
+        //
+        public async Task<Mission> GetMissionById(int id)
+        {
+            Mission mission = await _httpClient.GetFromJsonAsync<Mission>($"http://localhost:5125/missions/get/{id}");
+            return mission;
+        }
+
+    }
 }
